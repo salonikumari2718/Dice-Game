d4fd264ee152067f852b9f02d884efe25a40d659
salonikumari2718 <72732460+salonikumari2718@users.noreply.github.com>
2020-10-19 23:07:02 +0530

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,6 @@ if(randomNumber1===randomNumber2)
 }
 else if(randomNumber1>randomNumber2){
 document.querySelector("h1").textContent="🚩 Player 1 Wins!";
-
 }
 else{
   document.querySelector("h1").textContent="Player 2 Wins!🚩";
